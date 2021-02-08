7fe0f49af97c7ed2f312d116b8a7739c32ff8b71
Adiel Ribeiro <contato@nuvym.com>
2021-02-08 15:07:55 -0300

    hardened - By NUVYM

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodle';
+$CFG->dbpass    = '-#CHANGEME!P@ssw0rd';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '/var/run/mysql/mysql.sock',
+  'dbcollation' => 'utf8_general_ci',
+);
+
+$CFG->wwwroot   = 'http://35.172.230.130';
+$CFG->dataroot  = '/usr/local/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
