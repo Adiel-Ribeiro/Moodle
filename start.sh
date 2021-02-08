f3fe4551c05f94e58f71df121f131312e788b4a8
Adiel Ribeiro <contato@nuvym.com>
2021-02-08 13:56:46 -0300

    start

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+########################### start ec2 #######################################################
+su -m root -c "pw groupadd sudo" 
+su -m root -c "pw usermod ec2-user -G wheel,sudo,ssh,ec2-user"
+su -m root -c 'echo "%sudo ALL= ( ALL:ALL ) NOPASSWD: ALL" >> /usr/local/etc/sudoers'
+fetch -q --no-verify-peer https://raw.githubusercontent.com/Adiel-Ribeiro/Moodle/master/fstab
+su -m root -c "mv fstab /etc"
+su -m root -c "chmod 600 /etc/fstab"
+su -m root -c "reboot" 
+##############################################################################################
